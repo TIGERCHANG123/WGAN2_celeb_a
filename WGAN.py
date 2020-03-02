c1bf671f5da6a31c047c926a9a50efffcb9b3add
TIGERC123 <2462924814@qq.com>
2020-03-02 19:16:30 +0800

    large

--- WGAN.py
+++ WGAN.py
@@ -3,12 +3,12 @@ from WGAN_Block import *
 class generator(tf.keras.Model):
   def __init__(self, noise_dim):
     super(generator, self).__init__()
-    self.input_layer = generator_Input(shape=[4, 4, 512], noise_dim=noise_dim)
+    self.input_layer = generator_Input(shape=[4, 4, 1024], noise_dim=noise_dim)
 
     self.middle_layer_list = [
-      generator_Middle(filters=512, strides=2, padding='same'),#1024*4*4
-      generator_Middle(filters=256, strides=2, padding='same'),#512*8*8
-      generator_Middle(filters=128, strides=2, padding='same'),#256*16*16
+      generator_Middle(filters=1024, strides=2, padding='same'),#1024*4*4
+      generator_Middle(filters=512, strides=2, padding='same'),#512*8*8
+      generator_Middle(filters=256, strides=2, padding='same'),#256*16*16
     ]
 
     self.output_layer = generator_Output(image_depth=3, strides=2, padding='same')#3*32*32
@@ -22,11 +22,11 @@ class generator(tf.keras.Model):
 class discriminator(tf.keras.Model):
   def __init__(self):
     super(discriminator, self).__init__()
-    self.input_layer = discriminator_Input(filters=64, strides=2)
+    self.input_layer = discriminator_Input(filters=128, strides=2)
     self.middle_layer_list = [
-      discriminator_Middle(filters=128, strides=2, padding='same'),
       discriminator_Middle(filters=256, strides=2, padding='same'),
       discriminator_Middle(filters=512, strides=2, padding='same'),
+      discriminator_Middle(filters=1024, strides=2, padding='same'),
     ]
     self.output_layer = discriminator_Output(with_activation=False)
 
